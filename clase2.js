9996e0a7702898f797a74114415049b250a3c08f
Lenika Guerrero <lenikaguerrero@gmail.com>
2020-07-20 19:40:52 -0700

    Variables - Strings

---
+++ clase2.js
@@ -0,0 +1,13 @@
+var nombre ='Lenika', apellido = 'Guerrero';
+
+var nombreMayusculas=nombre.toUpperCase();
+var apellidoMinusculas=apellido.toLowerCase();
+
+var primeraLetraDelNombre=nombre.charAt(0);
+var cantidadDeLetrasDelNombre=nombre.length;
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+
+var str= nombre.substr(1,2);
+
+var ultimaLetra= nombre.charAt((nombre.length)-1);
