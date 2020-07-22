637cb37ecee9f7f4a7976710f6dc1e317ec4deb2
Lenika Guerrero <lenikaguerrero@gmail.com>
2020-07-21 20:59:06 -0700

    Comparaciones

---
+++ clase9.js
@@ -0,0 +1,17 @@
+var x=4,y='4';
+
+/*
+	(==): Convierte al mismo tipo de valor y permite que se puedan comparar. (Datos primitivos  Ej. Numérico y String)
+	(===): No iguala las variables que son de distinto tipo. Se usa para compara un mismo tipo de variable 
+*/
+
+
+var lenika ={
+	nombre:'Lenika'
+}
+
+/*var otraPersona= {
+	...lenika
+}*/
+
+var otraPersona=lenika;
