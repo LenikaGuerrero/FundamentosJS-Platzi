de506d6a4ea7e6cc5312223cdca74c70bf6b5390
Lenika Guerrero <lenikaguerrero@gmail.com>
2020-07-21 21:08:41 -0700

    Parametros como referencia o como valor

---
+++ Primeros Pasos en JavaScript/clase8.js
@@ -0,0 +1,28 @@
+var lenika ={
+	nombre:'Lenika',
+	apellido:'Guerrero',
+	edad:24
+}
+
+var sasha={
+	nombre:'Sasha',
+	apellido:'Lifszyc',
+	edad:28
+}
+
+function imprimirNombreMayusculas(persona){ 
+	//var nombre=persona.nombre;
+	var {nombre}=persona; //Nombre de la variable como parámetro de la segunda variable (para no duplicar variables)
+	console.log(nombre.toUpperCase());
+}
+
+imprimirNombreMayusculas(lenika);
+imprimirNombreMayusculas(sasha);
+//imprimirNombreMayusculas({nombre:'Brenda'});
+
+function cumpleanos(persona){
+	return{
+		...persona,
+		edad:persona.edad+1
+	}
+}//Devuelve un nuevo objeto como parametro
