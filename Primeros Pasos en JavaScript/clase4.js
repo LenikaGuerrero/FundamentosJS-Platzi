8ba7301d6730aaabee47cf8515404b49cacf7985
Lenika Guerrero <lenikaguerrero@gmail.com>
2020-07-21 21:06:31 -0700

    Funciones

---
+++ Primeros Pasos en JavaScript/clase4.js
@@ -0,0 +1,9 @@
+var nombre='Lenika', edad =23;
+
+function imprimirEdad(n,e){
+	console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre,edad);
+imprimirEdad('vicky',28);
+imprimirEdad('Erick',24);
