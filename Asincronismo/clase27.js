1f3efdb89a60b85b4cea123ec46a5e4daae63d55
Lenika Guerrero <lenikaguerrero@gmail.com>
2020-07-24 10:15:41 -0700

    Tiempo-setTimeout()

---
+++ Asincronismo/clase27.js
@@ -0,0 +1,17 @@
+//setTimeout() Recibe una funcion y el tiempo
+
+/*setTimeout(function(){
+    console.log('b');
+},2000)*/
+
+//console.log('a');
+//setTimeout(()=>console.log('b'),0);
+//console.log('c');
+
+
+
+//Manda la funcion a una cola de tareas y Se ejecuta cuando termina las tareas principales en el tiempo asignado
+setTimeout(()=>console.log('b'),2000);
+for(var i=0;i<1000000;i++){
+
+}
